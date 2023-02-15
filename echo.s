aa03e9b1314226edbe31b225425fa7a2eb839009
Prince of Devils <104875-attaboy@users.noreply.framagit.org>
2023-02-15 13:22:23 +0100

    A little bit of optimization
    xor rax, rax is always faster, than mov rax, 0, at least according to the interwebs ;-)

--- echo.s
+++ echo.s
@@ -2,20 +2,21 @@
 .globl _start
 
 # A table of Linux syscalls can be found here: https://x64.syscall.sh/
+# Instruction reference: https://www.felixcloutier.com/x86/
 
 .section .text
 
 _start:
 	sub rsp, 1024 # reserve stack space for buffer
 echo_loop:
-	mov rdi, 0    # stdin
+	xor rdi, rdi  # stdin
 	mov rsi, rsp  # pointer to buffer
 	mov rdx, 1024 # max len to read (buffer space)
-	mov rax, 0    # read syscall
+	xor rax, rax    # read syscall
 	syscall
 
+				 # rsi still set to buffer
 	mov rdi, 1   # stdout
-	mov rsi, rsp # pointer to read-in text
 	mov rdx, rax # len of read-in text
 	mov rax, 1   # write syscall
 	syscall
@@ -23,6 +24,6 @@ echo_loop:
 	jmp echo_loop
 
 	# unreachable
-	mov rdi, 0  # success status code
+	xor rdi, rdi  # success status code
 	mov rax, 60 # exit syscall
 	syscall
